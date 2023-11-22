19932ce06085360c638790af1f3076baafafabf9
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-11-22 23:16:03 +0600

    Express middlewire and router

--- project-1/dist/app/app.js
+++ project-1/dist/app/app.js
@@ -8,10 +8,39 @@ const app = (0, express_1.default)();
 //parser
 app.use(express_1.default.json());
 app.use(express_1.default.text());
-app.get("/", (req, res) => {
+// Router
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+userRouter.get("/create-user", (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "User created successfully",
+        data: user,
+    });
+});
+courseRouter.post("/create-course", (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "User created successfully",
+        data: course,
+    });
+});
+////////////////////////////////
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+app.get("/", logger, (req, res) => {
+    console.log(req.query);
     res.send("Hello World!");
 });
-app.post("/", (req, res) => {
+app.post("/", logger, (req, res) => {
     console.log(req.body);
     res.json({
         message: "successfully received data",
--- project-1/src/app/app.ts
+++ project-1/src/app/app.ts
@@ -1,15 +1,50 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 const app = express();
 
 //parser
 app.use(express.json());
 app.use(express.text());
 
-app.get("/", (req: Request, res: Response) => {
+// Router
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+
+userRouter.get("/create-user", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+  res.json({
+    success: true,
+    message: "User created successfully",
+    data: user,
+  });
+});
+
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+  res.json({
+    success: true,
+    message: "User created successfully",
+    data: course,
+  });
+});
+
+////////////////////////////////
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.hostname);
+  next();
+};
+
+app.get("/", logger, (req: Request, res: Response) => {
+  console.log(req.query);
   res.send("Hello World!");
 });
 
-app.post("/", (req: Request, res: Response) => {
+app.post("/", logger, (req: Request, res: Response) => {
   console.log(req.body);
   res.json({
     message: "successfully received data",
