fa590e1ad809661e02f6c265d100e6b83259b183
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-11-28 16:27:04 +0600

    file management

--- project-2/src/app.ts
+++ project-2/src/app.ts
@@ -1,16 +1,14 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
+import express, { Response, Request, Application } from "express";
+import cors from "cors";
 const app: Application = express();
 
-// Parsers
+// parsers
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/v1/students', StudentRoutes);
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.get("/", (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
 });
 
 export default app;
