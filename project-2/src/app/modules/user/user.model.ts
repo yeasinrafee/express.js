15fc8950f2e56f6d9659cde32ba507ba6324f908
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-12-03 16:16:51 +0600

    user file setup, interface and schema model created

---
+++ project-2/src/app/modules/user/user.model.ts
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+import { TUser } from "./user.interface";
+
+const userSchema = new Schema<TUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "student", "faculty"],
+    },
+    status: {
+      type: String,
+      enum: ["in-progress", "blocked"],
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// User model
+export const User = model<TUser>("User", userSchema);
