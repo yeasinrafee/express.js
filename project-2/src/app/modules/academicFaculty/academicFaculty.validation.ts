2603fae32a90cf811aa947c4225a3b1cdf22d4db
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-12-25 10:06:08 +0600

    Academic faculty interface, model, validation

---
+++ project-2/src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+const academicFacultyValidationSchema = z.object({
+  name: z.string({
+    invalid_type_error: "Academic faculty must be string",
+  }),
+});
+
+export const UserValidation = {
+  academicFacultyValidationSchema,
+};
--- project-2/src/app/modules/user/user.utils.ts
+++ project-2/src/app/modules/user/user.utils.ts
@@ -16,11 +16,26 @@ const findLastStudent = async () => {
     })
     .lean();
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudent()) || (0).toString();
+  let currentId = (0).toString(); // 0000 by default
+
+  const lastStudentId = await findLastStudent();
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
 
   let incrementId = (Number(currentId) + 1).toString().padStart(4, "0");
 
