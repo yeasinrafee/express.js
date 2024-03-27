d6b269049a19e7c9652813dcb726fa9be232ff90
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-03-28 00:12:41 +0600

    Searching student through email, firstname and presentAddress

--- project-2/src/app/modules/student/student.service.ts
+++ project-2/src/app/modules/student/student.service.ts
@@ -5,8 +5,16 @@ import httpStatus from "http-status";
 import { User } from "../user/user.model";
 import { TStudent } from "./student.interface";
 
-const getAllStudentsFromDB = async () => {
-  const result = await Student.find()
+const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
+  let searchTerm = "";
+  if (query?.searchTerm) {
+    searchTerm = query?.searchTerm as string;
+  }
+  const result = await Student.find({
+    $or: ["email", "name.firstName", "presentAddress"].map((field) => ({
+      [field]: { $regex: searchTerm, $options: "i" },
+    })),
+  })
     .populate("admissionSemester")
     .populate({
       path: "academicDepartment",
