9242be3160f8e2a34cef1430f35d2defcf322a08
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-04-06 10:33:09 +0600

    Course service controller and validation

---
+++ project-2/src/app/modules/course/course.service.ts
@@ -0,0 +1,33 @@
+import { TCourse } from "./course.interface";
+import { Course } from "./course.model";
+
+const createCourseIntoDB = async (payload: TCourse) => {
+  const result = await Course.create(payload);
+  return result;
+};
+
+const getAllCoursesFromDB = async () => {
+  const result = await Course.find();
+  return result;
+};
+
+const getSingleCourseFromDB = async (id: string) => {
+  const result = await Course.findById(id);
+  return result;
+};
+
+const deleteCourseFromDB = async (id: string) => {
+  const result = await Course.findByIdAndUpdate(
+    id,
+    { isDelete: true },
+    { new: true }
+  );
+  return result;
+};
+
+export const CourseServices = {
+  createCourseIntoDB,
+  getAllCoursesFromDB,
+  getSingleCourseFromDB,
+  deleteCourseFromDB,
+};
---
+++ project-2/src/app/modules/course/course.validation.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+const preRequisiteCoursesValidationSchema = z.object({
+  course: z.string(),
+  isDeleted: z.boolean().optional(),
+});
+
+const createCourseVAlidationSchema = z.object({
+  body: z.object({
+    title: z.string(),
+    prefix: z.string(),
+    code: z.number(),
+    credits: z.number(),
+    preRequisiteCourses: z.array(preRequisiteCoursesValidationSchema),
+  }),
+});
+
+export const CourseVAlidation = {
+  createCourseVAlidationSchema,
+};
