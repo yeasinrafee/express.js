989603afce4aa3de66cc7b88e6c297a99e382ccb
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-08-26 16:59:04 +0600

    Creating refresh token in login and sent to the client

--- project-2/src/app/modules/Auth/auth.controller.ts
+++ project-2/src/app/modules/Auth/auth.controller.ts
@@ -2,14 +2,24 @@ import httpStatus from 'http-status';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { AuthService } from './auth.service';
+import config from '../../config';
 
 const loginUser = catchAsync(async (req, res) => {
   const result = await AuthService.loginUser(req.body);
+  const { refreshToken, accessToken, needsPasswordChange } = result;
+
+  res.cookie('refreshToken', refreshToken, {
+    secure: config.NODE_ENV === 'production',
+    httpOnly: true,
+  });
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'User in logged in successfully!',
-    data: result,
+    data: {
+      accessToken,
+      needsPasswordChange,
+    },
   });
 });
 
--- project-2/src/app/modules/Auth/auth.service.ts
+++ project-2/src/app/modules/Auth/auth.service.ts
@@ -5,6 +5,7 @@ import { TLoginUser } from './auth.interface';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../../config';
 import bcrypt from 'bcrypt';
+import { createToken } from './auth.utils';
 
 const loginUser = async (payload: TLoginUser) => {
   const user = await User.isUserExistByCustomId(payload.id);
@@ -36,12 +37,21 @@ const loginUser = async (payload: TLoginUser) => {
     role: user.role,
   };
   // create token and sent to the client
-  const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret as string, {
-    expiresIn: '10d',
-  });
+  const accessToken = createToken(
+    jwtPayload,
+    config.jwt_access_secret as string,
+    config.jwt_access_expires_in as string
+  );
+  // create token and sent to the client
+  const refreshToken = createToken(
+    jwtPayload,
+    config.jwt_refresh_secret as string,
+    config.jwt_refresh_expires_in as string
+  );
 
   return {
     accessToken,
+    refreshToken,
     needsPasswordChange: user?.needsPasswordChange,
   };
 };
